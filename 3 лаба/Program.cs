6a2a1959847adcd3490687d34086d245d527b97e
админ <@DESKTOP-MDU52T5>
2023-11-29 14:09:08 +0300

    Добавьте файлы проекта.

---
+++ 3 лаба/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int factorial = 1;
+for (int i = 1; i <= x; i++)
+{
+    factorial = factorial * i;  
+}
+Console.WriteLine(factorial);
